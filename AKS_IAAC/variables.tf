ffdaa65152ecddaf2472f95860e382d3d6a47bdf
Haseef-Ahamed <163092499+Haseef-Ahamed@users.noreply.github.com>
2025-03-11 12:06:03 +0530

    variables

---
+++ AKS_IAAC/variables.tf
@@ -0,0 +1,16 @@
+variable "resource_group_name" {
+  default = "aks-resource-group"
+}
+
+variable "location" {
+  default = "Southeast Asia"
+}
+
+variable "cluster_name" {
+  default = "myAKSCluster"
+}
+
+variable "node_count" {
+  default = 2
+}
+
